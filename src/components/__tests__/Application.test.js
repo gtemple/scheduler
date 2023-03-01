9804f7c6a9a6e14d50f5a3c746fa862c50042f4c
Giordano Temple <giordanotemple@gmail.com>
2023-02-28 20:45:20 -0500

    add cypress testing

--- src/components/__tests__/Application.test.js
+++ src/components/__tests__/Application.test.js
@@ -1,13 +1,46 @@
 import React from "react";
 
-import { render, cleanup } from "@testing-library/react";
+import { render,
+    cleanup,
+    waitForElement,
+    fireEvent,
+    getByText,
+    prettyDOM,
+    getAllByTestId,
+    getByAltText,
+    getByPlaceholderText } from "@testing-library/react";
 
 import Application from "components/Application";
 
 afterEach(cleanup);
+describe("Appointment", () => {
+  it("defaults to Monday and changes the schedule when a new day is selected", () => {
+    const { getByText } = render(<Application />);
 
-describe("Application", () => {
-  it("renders without crashing", () => {
-    render(<Application />);
+    return waitForElement(() => getByText("Monday")).then(() => {
+      fireEvent.click(getByText("Tuesday"));
+      expect(getByText("Tuesday")).toBeInTheDocument();
+    });
   });
+  it("loads data, books an interview and reduces the spots remaining for Monday by 1", async () => {
+    const { container } = render(<Application />);
+  
+    await waitForElement(() => getByText(container, "Archie Cohen"));
+  
+    const appointments = getAllByTestId(container, "appointment");
+    const appointment = appointments[0];
+  
+    fireEvent.click(getByAltText(appointment, "Add"));
+  
+    fireEvent.change(getByPlaceholderText(appointment, /enter student name/i), {
+      target: { value: "Lydia Miller-Jones" }
+    });
+    fireEvent.click(getByAltText(appointment, "Sylvia Palmer"));
+  
+    fireEvent.click(getByText(appointment, "Save"));
+  
+    console.log(prettyDOM(appointment));
+  });
+  
+  
 })
