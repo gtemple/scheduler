086a85502482dcb54249388da03c41f6c8651096
Giordano Temple <giordanotemple@gmail.com>
2023-03-01 14:47:23 -0500

    fix bug where selected interviewer was not default when editing a form

--- src/components/Appointment/Header.js
+++ src/components/Appointment/Header.js
@@ -1,7 +1,6 @@
 import React from "react";
 
 export default function Header(props) {
-  const  { student, interviewer, onEdit, onDelete } = props;
   return (
     <header className="appointment__time">
       <h4 className="text--semi-bold">{props.time}</h4>
